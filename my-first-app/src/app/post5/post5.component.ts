8799ee85483e34a6650996a126eff89692181f39
Valentina Mikhailova <valentina.mikhailova@office.e2e4gu.ru>
2023-08-31 10:14:11 +0700

    Working with ngSwitch, ngFor and Pipes

--- my-first-app/src/app/post5/post5.component.ts
+++ my-first-app/src/app/post5/post5.component.ts
@@ -11,7 +11,23 @@ export class Post5Component {
   title2 = 'Initial2';
 
   backgroundToggle = false;
-  toggle = false;
+  toggle: boolean | string = false;
+
+  now: Date = new Date();
+
+  arr = ['A', 'B', 'C','D', 'E', 'F', 'G'];
+  objs = [
+    {title: 'Post 1', author: 'Valentina', comments: [
+      {name: 'Max', text: 'Lorem 1'},
+      {name: 'Max', text: 'Lorem 2'},
+      {name: 'Max', text: 'Lorem 3'},
+    ]},
+    {title: 'Post 2', author: 'Valentina M', comments: [
+      {name: 'Man', text: 'Lorem 1'},
+      {name: 'Man', text: 'Lorem 2'},
+      {name: 'Man', text: 'Lorem 3'},
+    ]}
+  ]
 
   onInput(event: any) {
     this.title = event.target.value;
