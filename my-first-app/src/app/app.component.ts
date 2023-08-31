9eab89f797fec90f07245d43a784e928efd64f6b
Valentina Mikhailova <valentina.mikhailova@office.e2e4gu.ru>
2023-08-31 17:55:44 +0700

    Form groups, dynamic data update, FormArray operations

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -1,15 +1,16 @@
 import {Component, OnInit} from '@angular/core'
-import { FormControl, FormGroup, Validators } from '@angular/forms'
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   form: FormGroup;
+  skills: FormArray;
 
-  ngOnInit(): void {
+  constructor() {
     this.form = new FormGroup({
       email: new FormControl('', [
         Validators.email, 
@@ -18,18 +19,47 @@ export class AppComponent implements OnInit {
       password: new FormControl(null, [
         Validators.required,
         Validators.minLength(6)
-      ])
+      ]),
+      address: new FormGroup({
+        country: new FormControl('ru'),
+        city: new FormControl('', Validators.required)
+      }),
+      skills: new FormArray([])
     });
+
+    this.skills = this.form.get('skills') as FormArray;
+  }
+
+  setCapital() {
+
+    const cityMap = {
+      ru: 'Москва',
+      ua: 'Киев',
+      by: 'Минск'
+    }
+
+    const cityKey: 'ru' | 'ua' | 'by' = this.form.get('address').get('country').value;
+    const city = cityMap[cityKey];
+
+    this.form.patchValue({address: {city}});
+  }
+
+  addSkill() {
+
+    const control = new FormControl('', Validators.required);
+    this.skills.push(control);
+
+    // (this.form.get('skills') as FormArray).push(control);
+    // (<FormArray>this.form.get('skills')).push();
   }
 
   submit() {
 
     if (this.form.valid) {
-      
+
       console.log('Form:', this.form);
   
       const formData = {...this.form.value};
-      
       console.log('Form Data:', formData);
     }
   }
