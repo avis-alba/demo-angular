268f7f5fb1e702f089181431d74fcc4ea967d9c1
Valentina Mikhailova <valentina.mikhailova@office.e2e4gu.ru>
2023-09-01 18:06:13 +0700

    Fix HTML structure for fields, edit CSS, add basic validators & error messages

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -8,5 +8,23 @@ import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms'
 })
 export class AppComponent {
   
+  form: FormGroup;
+
+  constructor() {
+    this.form = new FormGroup({
+      name: new FormControl(null, [Validators.required, Validators.maxLength(50)]),
+      lastName: new FormControl(null, [Validators.required, Validators.maxLength(50)]),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      birthDate: new FormControl(null, [Validators.required]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(8)]),
+      confirmPassword: new FormControl(null, [Validators.required])
+    });
+  }
+
+  public submit():void {
+
+    console.log(this.form.value);
+  }
+
 }
 
--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -5,7 +5,7 @@ import {NgModule} from '@angular/core'
 import {AppComponent} from './app.component'
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import {MAT_FORM_FIELD_DEFAULT_OPTIONS, MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
@@ -30,7 +30,8 @@ import {MatNativeDateModule, MAT_DATE_LOCALE} from '@angular/material/core';
     MatNativeDateModule
   ],
   providers: [
-    {provide: MAT_DATE_LOCALE, useValue: 'ru-RU'}
+    { provide: MAT_DATE_LOCALE, useValue: 'ru-RU' },
+    { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { subscriptSizing: 'dynamic' } }
   ],
   bootstrap: [AppComponent]
 })
