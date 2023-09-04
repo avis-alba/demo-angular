609eae9c78fecfc42f3bba282f4eb1a1d93db02d
Valentina Mikhailova <valentina.mikhailova@office.e2e4gu.ru>
2023-09-04 13:36:07 +0700

    Validators for name, last name, email, date & password confirmation

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -1,8 +1,7 @@
 import {BrowserModule} from '@angular/platform-browser'
 import {NgModule} from '@angular/core'
 
-
-import {AppComponent} from './app.component'
+import {AppComponent, MOMENT_DATE_FORMATS} from './app.component'
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MAT_FORM_FIELD_DEFAULT_OPTIONS, MatFormFieldModule} from '@angular/material/form-field';
@@ -11,7 +10,8 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatNativeDateModule, MAT_DATE_LOCALE} from '@angular/material/core';
-
+import {DateAdapter, MAT_DATE_FORMATS} from '@angular/material/core';
+import {MomentDateAdapter, MAT_MOMENT_DATE_ADAPTER_OPTIONS} from '@angular/material-moment-adapter'
 
 @NgModule({
   declarations: [
@@ -31,7 +31,13 @@ import {MatNativeDateModule, MAT_DATE_LOCALE} from '@angular/material/core';
   ],
   providers: [
     { provide: MAT_DATE_LOCALE, useValue: 'ru-RU' },
-    { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { subscriptSizing: 'dynamic' } }
+    { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { subscriptSizing: 'dynamic' } },
+    {
+      provide: DateAdapter,
+      useClass: MomentDateAdapter,
+      deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS],
+    },
+    {provide: MAT_DATE_FORMATS, useValue: MOMENT_DATE_FORMATS},
   ],
   bootstrap: [AppComponent]
 })
