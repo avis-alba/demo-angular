fe35584736f964d613e6a8c7679c4a1c1e10e86d
Valentina Mikhailova <valentina.mikhailova@office.e2e4gu.ru>
2023-09-13 18:23:55 +0700

    Budget table templates, html & css

--- my-first-app/src/app/components/budget/budget.component.ts
+++ my-first-app/src/app/components/budget/budget.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatTable } from '@angular/material/table';
 
 @Component({
   selector: 'app-budget',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./budget.component.scss']
 })
 export class BudgetComponent {
+  
+    public displayedColumns: string[] = ['category', 'item', 'amount', 'percent', 'tools'];
+    public incomeData: any[] = [{1: 'd'}, {2: 'f'}];
+    public outcomeData: any[] = [1, 2, 3, 4, 5];
 
+    @ViewChild(MatTable) incomeTable: MatTable<any>;
+    @ViewChild(MatTable) outcomeTable: MatTable<any>;
+
+    constructor() {
+      
+    }
 }
